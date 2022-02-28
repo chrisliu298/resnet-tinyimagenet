badd10b20b02c31e3751c6202a775039d94a9288
chrisliu298 <yuhaoliu298@gmail.com>
2022-02-27 16:16:11 -0800

    Add more models

--- model.py
+++ model.py
@@ -1,36 +1,67 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision
 from pytorch_lightning import LightningModule
 from torch.optim.lr_scheduler import OneCycleLR
 from torchmetrics.functional import accuracy
+from torchvision.models import (
+    resnet18,
+    resnet34,
+    resnet50,
+    resnet101,
+    resnet152,
+    resnext50_32x4d,
+    resnext101_32x8d,
+    wide_resnet50_2,
+    wide_resnet101_2,
+)
+
+resnet_configs = {
+    "resnet18": resnet18,
+    "resnet34": resnet34,
+    "resnet50": resnet50,
+    "resnet101": resnet101,
+    "resnet152": resnet152,
+    "resnext50": resnext50_32x4d,
+    "resnext101": resnext101_32x8d,
+    "wide_resnet50": wide_resnet50_2,
+    "wide_resnet101": wide_resnet101_2,
+}
 
 
 class ResNet(LightningModule):
-    def __init__(self, lr=0.1, batch_size=128):
+    def __init__(
+        self,
+        model_name="resnet18",
+        pretrained=True,
+        output_size=200,
+        lr=0.1,
+        batch_size=128,
+        weight_decay=1e-4,
+        replace_conv1=True,
+        replace_maxpool=True,
+    ):
         super().__init__()
         self.save_hyperparameters()
-        self.model = torchvision.models.resnet18(pretrained=True)
-        self.model.conv1 = nn.Conv2d(
-            3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False
+        assert self.hparams.model_name in resnet_configs.keys()
+        self.model = resnet_configs[self.hparams.model_name](
+            pretrained=self.hparams.pretrained
         )
-        self.model.maxpool = nn.Identity()
-        self.model.fc = nn.Linear(512, 200)
+        if self.hparams.replace_conv1:
+            self.model.conv1 = nn.Conv2d(
+                3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False
+            )
+        if self.hparams.replace_maxpool:
+            self.model.maxpool = nn.Identity()
+        self.model.fc = (
+            nn.Linear(512, self.hparams.output_size)
+            if self.hparams.model_name in ["resnet18", "resnet34"]
+            else nn.Linear(2048, self.hparams.output_size)
+        )
 
     def forward(self, x):
         out = self.model(x)
         return out
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        output = self(x)
-        pred = torch.argmax(output, dim=1)
-        loss = F.cross_entropy(output, y)
-        acc = accuracy(pred, y)
-        self.log("train_loss", loss, logger=True)
-        self.log("train_acc", acc, logger=True)
-        return {"loss": loss, "train_acc": acc}
 
     def evaluate(self, batch, stage=None):
         x, y = batch
@@ -43,20 +74,42 @@ class ResNet(LightningModule):
             self.log(f"{stage}_acc", acc, logger=True, prog_bar=True)
         return loss, acc
 
+    def training_step(self, batch, batch_idx):
+        loss, acc = self.evaluate(batch, "train")
+        return {"loss": loss, "train_acc": acc}
+
+    def training_epoch_end(self, outputs):
+        avg_loss = torch.stack([i["loss"] for i in outputs]).mean()
+        avg_acc = torch.stack([i["train_acc"] for i in outputs]).mean()
+        self.log("avg_train_loss", avg_loss, logger=True)
+        self.log("avg_train_acc", avg_acc, logger=True)
+
     def validation_step(self, batch, batch_idx):
         loss, acc = self.evaluate(batch, "val")
         return {"val_loss": loss, "val_acc": acc}
 
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([i["val_loss"] for i in outputs]).mean()
+        avg_acc = torch.stack([i["val_acc"] for i in outputs]).mean()
+        self.log("avg_val_loss", avg_loss, logger=True)
+        self.log("avg_val_acc", avg_acc, logger=True)
+
     def test_step(self, batch, batch_idx):
         loss, acc = self.evaluate(batch, "test")
         return {"test_loss": loss, "test_acc": acc}
+
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack([i["test_loss"] for i in outputs]).mean()
+        avg_acc = torch.stack([i["test_acc"] for i in outputs]).mean()
+        self.log("avg_test_loss", avg_loss, logger=True)
+        self.log("avg_test_acc", avg_acc, logger=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(
             self.parameters(),
             lr=self.hparams.lr,
             momentum=0.9,
-            weight_decay=5e-4,
+            weight_decay=self.hparams.weight_decay,
         )
         steps_per_epoch = 100000 // self.hparams.batch_size + 1
         scheduler_dict = {
--- train.py
+++ train.py
@@ -1,29 +1,58 @@
 import os
 
+import wandb
+from easydict import EasyDict
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import LearningRateMonitor, TQDMProgressBar
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import WandbLogger
 
 from dataset import DataModule
-from model import ResNet
+from model import ResNet, resnet_configs
 
-seed_everything(12345)
-batch_size = 128
-num_workers = int(os.cpu_count() / 2)
+for model_name in resnet_configs.keys():
+    config = EasyDict(
+        model_name=model_name,
+        pretrained=True,
+        output_size=200,
+        replace_conv1=True,
+        replace_maxpool=True,
+        lr=0.1,
+        max_epochs=50,
+        weight_decay=1e-4,
+        batch_size=128,
+        seed=12345,
+        num_workers=int(os.cpu_count() / 2),
+    )
+    seed_everything(config.seed)
+    wandb.init(project="resnet-tinyimagenet", entity="chrisliu298", config=config)
 
-datamodule = DataModule(batch_size=batch_size, num_workers=num_workers)
-model = ResNet(lr=0.1, batch_size=batch_size)
+    datamodule = DataModule(
+        batch_size=config.batch_size, num_workers=config.num_workers
+    )
+    model = ResNet(
+        model_name=config.model_name,
+        pretrained=config.pretrained,
+        output_size=config.output_size,
+        lr=config.lr,
+        batch_size=config.batch_size,
+        weight_decay=config.weight_decay,
+        replace_conv1=config.replace_conv1,
+        replace_maxpool=config.replace_maxpool,
+    )
 
-trainer = Trainer(
-    gpus=-1,
-    max_epochs=50,
-    logger=TensorBoardLogger("lightning_logs/", name="resnet"),
-    callbacks=[
-        LearningRateMonitor(logging_interval="step"),
-        TQDMProgressBar(refresh_rate=10),
-    ],
-    benchmark=True,
-)
-
-trainer.fit(model, datamodule=datamodule)
-result = trainer.test(model, datamodule=datamodule)
+    trainer = Trainer(
+        gpus=-1,
+        max_epochs=config.max_epochs,
+        logger=WandbLogger(
+            save_dir="wandb/",
+            project="tiny-imagenet",
+        ),
+        callbacks=[
+            LearningRateMonitor(logging_interval="step"),
+            TQDMProgressBar(refresh_rate=10),
+        ],
+        benchmark=True,
+    )
+    trainer.fit(model, datamodule=datamodule)
+    result = trainer.test(model, datamodule=datamodule)
+    wandb.finish(quiet=True)
